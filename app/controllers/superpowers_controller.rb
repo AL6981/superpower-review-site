3b9ec080148c6a942b6f46fab4c7a2b954ec01d3
Khanh Le <vikhanhle@gmail.com>
2018-01-25 16:10:17 -0500

    refactor admin

--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -8,47 +8,4 @@ class SuperpowersController < ApplicationController
   def show
     @superpower = Superpower.find(params[:id])
   end
-
-  def new
-    @superpower = Superpower.new
-  end
-
-  def create
-    @superpower = Superpower.new(superpower_params)
-    @superpower.user = current_user
-
-    if @superpower.save
-      flash[:notice] = 'Superpower added successfully'
-      redirect_to superpower_path(@superpower)
-    else
-      render :new
-    end
-  end
-
-  def edit
-    @superpower = Superpower.find(params[:id])
-  end
-
-  def update
-    @superpower = Superpower.find(params[:id])
-
-    if @superpower.update_attributes(superpower_params)
-      flash[:notice] = 'Superpower successfully updated'
-      redirect_to superpower_path(@superpower)
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    Superpower.find(params[:id]).destroy
-    flash[:success] = 'Superpower successfully deleted'
-    redirect_to superpowers_path
-  end
-
-  protected
-
-  def superpower_params
-    params.require(:superpower).permit(:name, :description)
-  end
 end
