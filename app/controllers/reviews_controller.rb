36b5712487a7662c4eb5c7e57c7fb3693580db0f
Dennis Tung <detung@gmail.com>
2018-01-26 11:14:29 -0500

    user authorization for edit/update & destroy review

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,67 @@
+class ReviewsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def new
+    @superpower = Superpower.find(params[:superpower_id])
+    @review = Review.new
+  end
+
+  def create
+    @superpower = Superpower.find(params[:superpower_id])
+    @review = Review.new(review_params)
+    @review.user = current_user
+    @review.superpower = @superpower
+
+    if @review.save
+      flash[:notice] = 'Review Added Successfully'
+      redirect_to superpower_path(@superpower)
+    else
+      flash[:alert] = @review.errors.full_messages.join(", ")
+      render :new
+    end
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+
+    if @review.update_attributes(review_params)
+      flash[:notice] = 'Review Updated Successfully'
+      redirect_to superpower_path(@superpower)
+    else
+      flash[:alert] = @review.errors.full_messages.join(", ")
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @superpower = @review.superpower
+
+    @review.destroy
+
+    flash[:notice] = 'Review deleted'
+
+    redirect_to superpower_path(@superpower)
+
+  end
+
+  private
+
+  def authorize_user
+     @review = Review.find(params[:id])
+     @superpower = @review.superpower
+
+    if !user_signed_in? || current_user != review.user
+      flash[:notice] = 'Unauthorized access'
+      redirect_to superpower_path(@superpower)
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :body)
+  end
+end
