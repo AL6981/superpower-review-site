11660a9c27dd25fc5f3aab95c1b4677cdb892d0f
rbarrett84 <rbarrett84@yahoo.com>
2018-01-30 10:09:36 -0500

    Updated app.js file

--- app/javascript/react/containers/App.js
+++ app/javascript/react/containers/App.js
@@ -1,1 +1,12 @@
-<h1> Hello from the best group ever! </h1>
+import React from 'react'
+
+
+const App = props => {
+  return (
+    <div>
+      <h1> Hello from the best group ever! </h1>
+    </div>
+  )
+}
+
+export default App
