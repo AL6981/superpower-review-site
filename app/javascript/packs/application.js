3a534ff4e152b71515241e005bf4f8c8b18921ce
Khanh Le <vikhanhle@gmail.com>
2018-01-30 10:42:40 -0500

    created superpower show container

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,9 +10,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../react/containers/App'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-);
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+  );
+})
 
 console.log('Hello World from Webpacker')
