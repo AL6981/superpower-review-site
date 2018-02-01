1b1fc0c38710c3df0e80d06212172f23ade6393f
rbarrett84 <rbarrett84@yahoo.com>
2018-02-01 13:56:07 -0500

    Made some styling changes to the index page

--- app/javascript/react/components/SuperpowerTile.js
+++ app/javascript/react/components/SuperpowerTile.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router';
 
 const SuperpowerTile = props => {
   return(
-    <div>
+    <div className="superpower-tile">
       <p><Link to={`/superpowers/${props.id}`}>{props.name}</Link></p>
     </div>
   )
--- app/javascript/react/containers/SuperpowerIndexContainer.js
+++ app/javascript/react/containers/SuperpowerIndexContainer.js
@@ -29,7 +29,7 @@ class SuperpowerIndexContainer extends Component {
     })
 
     return(
-      <div>
+      <div className= "header">
         {superpowers}
       </div>
     )
