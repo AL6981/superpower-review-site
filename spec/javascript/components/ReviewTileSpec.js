9407221e66ae7d1c54b66ce80b5aab2e45113bee
[AL6981] <amylynnarrington@gmail.com>
2018-02-01 19:10:37 -0500

    fixed tests

--- spec/javascript/components/ReviewTileSpec.js
+++ spec/javascript/components/ReviewTileSpec.js
@@ -12,9 +12,9 @@ describe('ReviewTile', () => {
     )
   });
 
-  it('renders a div element with class name of "review-tile" and have rating and body text', () => {
-    expect(wrapper.find('.review-tile')).toBePresent();
-    expect(wrapper.find('.review-tile')).toMatchSelector('div');
+  it('renders a div element with class name of "review-tile-component" and have rating and body text', () => {
+    expect(wrapper.find('.review-tile-component')).toBePresent();
+    expect(wrapper.find('.review-tile-component')).toMatchSelector('div');
     expect(wrapper.find('.review-rating')).toIncludeText('2')
     expect(wrapper.find('.review-body')).toIncludeText('This is a review')
   });
